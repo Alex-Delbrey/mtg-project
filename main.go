525d08f3537025be4834564717225d9e77fe722b
Jandro <delbreyalex@gmail.com>
2025-03-08 20:16:20 -0400

    findings

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	// "github.com/gin-gonic/gin"
+)
+
+const mtgApi = "https://api.magicthegathering.io/v1/cards"
+
+func main() {
+	c, err := http.Get(mtgApi)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(c)
+}
