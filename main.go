3a7c443a5561e268a13a2030ab375506e5c77a34
Jandro <delbreyalex@gmail.com>
2025-03-08 21:10:45 -0400

    learning how to read json with go

--- main.go
+++ main.go
@@ -6,13 +6,14 @@ import (
 	// "github.com/gin-gonic/gin"
 )
 
-const mtgApi = "https://api.magicthegathering.io/v1/cards"
+const mtgApi = "https://api.magicthegathering.io/v1/cards?name=%s"
 
 func main() {
-	c, err := http.Get(mtgApi)
+	c, err := http.Get(fmt.Sprintf(mtgApi, "Ancestor's Chosen"))
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	defer c.Body.Close()
 	fmt.Println(c)
 }
